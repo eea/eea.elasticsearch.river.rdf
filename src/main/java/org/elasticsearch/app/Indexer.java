e9a097a7f20a578687356b144000f1a01b6869ed
Adrian Apostol <dev.adrianapostol@gmail.com>
2018-01-30 13:58:27 +0200

    Refs #89516 - created main app class

---
+++ src/main/java/org/elasticsearch/app/Indexer.java
@@ -0,0 +1,12 @@
+package  org.elasticsearch.app;
+
+import org.elasticsearch.river.eea_rdf.support.Harvester;
+
+public class Indexer {
+    public static void main(String[] args){
+        Harvester harvester = new Harvester();
+        //harvester.run();
+
+
+    }
+}
