f82d055d64cbbf4a73144ccc0940f391a4e6d07a
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2019-06-12 12:32:53 +0300

    Updated startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
-printenv | grep -v "no_proxy" >> /etc/environment
-rm -f /var/run/rsyslogd.pid
-rm -f /var/run/indexer.pid
-/usr/sbin/service rsyslog start
-crontab /tmp/crontab
-/usr/sbin/cron -f
+sh -c `printenv | grep -v "no_proxy" >> /etc/environment && rm -f /var/run/rsyslogd.pid && rm -f /var/run/indexer.pid && /usr/sbin/service rsyslog start && crontab /tmp/crontab && /usr/sbin/cron -f`
